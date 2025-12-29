be30314b32948d72310cd8c81bd25d95a139bb2c
Vollerei Ch. <116440938+AdnNyx@users.noreply.github.com>
2025-12-29 22:24:27 +0700

    Refactor app.py to include necessary imports

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 # Add render_template to imports
 from flask import Flask, request, jsonify, render_template
 import yt_dlp
@@ -68,4 +69,5 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
 
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
